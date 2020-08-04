087572f7d74c06fc65a765c6a4883c7d62f38d50
Zac <ztaylor555@gmail.com>
2020-08-04 05:03:45 -0400

    wp-scss issue

---
+++ wp-content/themes/ActorsAlpha/gulpfile.babel.js
@@ -0,0 +1,91 @@
+import gulp from 'gulp';
+import yargs from 'yargs';
+import sass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
+import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+import del from 'del';
+import webpack from 'webpack-stream';
+import named from 'vinyl-named';
+import browserSync from 'browser-sync';
+import zip from 'gulp-zip';
+import replace from 'gulp-replace';
+import info from './package.json';
+
+const server = browserSync.create();
+const PROD = yargs.argv.prod;
+
+export const serve = (done) => {
+    server.init({
+        proxy: "https://digitalzac.dev.cc/"
+    });
+    done();
+}
+
+export const reload = (done) => {
+    server.reload();
+    done();
+}
+
+const paths = { //refactored style paths.
+    styles: {
+        src:['src/assets/scss/bundle.scss','src/assets/scss/admin.scss','src/assets/lib/bulma.min.css'],
+        dest:'dist/assets/css' 
+    },
+    scripts: {
+        src: ['src/assets/js/bundle.js', 'src/assets/js/admin.js'],
+        dest: 'dist/assets/js'
+    },
+    images: {
+        src: 'src/assets/img/**/*.{jpg,jpeg,png,svg,gif}',
+        dest: 'dist/assets/img'
+    },
+    package:{
+        src: ['**/*','!src{,/**}', '!.vscode', '!node_modules{,/**}', '!packaged{,/**}', '!.babelrc', '!.gitignore', '!gulpfile.babel.js', '!package.json', '!package-lock.json' ],
+        dest:'packaged'
+    }
+}
+
+export const styles = (done) =>{
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!PROD, sourcemaps.init()))
+            .pipe(gulpif(PROD, cleanCSS({compatibility: 'ie8'})))
+            .pipe(sass().on('error', sass.logError))
+        .pipe(gulpif(!PROD, sourcemaps.write()))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(server.stream());
+    done();
+}
+
+export const watch = () => {
+    gulp.watch('src/assets/scss/**/*.scss', styles);
+    gulp.watch('src/assets/js/**/*.js', gulp.series(scripts, reload));
+    gulp.watch('**/*.php',reload);
+    gulp.watch(paths.images.src,  gulp.series(images, reload));
+}
+
+export const images = () => {
+    return gulp.src(paths.images.src)
+        .pipe(gulpif(PROD, imagemin()))
+        .pipe(gulp.dest(paths.images.dest));
+}
+
+export const clean = () => {
+    return del(['dist']);
+}
+
+export const compress = () => {
+    return gulp.src(paths.package.src)
+        .pipe(replace('_themename', info.name))
+        .pipe(zip(`${info.name}.zip`))
+        .pipe(gulp.dest(paths.package.dest));
+}
+
+export const build = gulp.series(clean, gulp.parallel(styles, scripts, images));
+
+export const dev = gulp.series(clean, gulp.parallel(styles, scripts, images), serve, watch);
+
+export const bundle = gulp.series(build, compress);
+
+export default dev;
